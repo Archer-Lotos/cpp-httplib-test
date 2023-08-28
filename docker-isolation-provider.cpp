8e464b60ad852ba8f90fb736f13280cab3bd9689
Viacheslav <novikova835@gmail.com>
2023-08-28 12:20:32 +0300

    fix make

--- docker-isolation-provider.cpp
+++ docker-isolation-provider.cpp
@@ -1,14 +1,13 @@
-#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "httplib.h"
 
-// HTTP
-httplib::Server svr;
+int main(void) {
+    using namespace httplib;
 
-// HTTPS
-httplib::SSLServer svr;
+    Server svr;
 
-svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
-res.set_content("Test!", "text/plain");
-});
+    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Test!", "text/plain");
+    });
 
-svr.listen("0.0.0.0", 8080);
+    svr.listen("0.0.0.0", 8080);
+}
